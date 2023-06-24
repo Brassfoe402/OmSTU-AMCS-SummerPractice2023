55aad5727755a6149d5cafd2de796b1897509505
Brassfoe402 <125663981+Brassfoe402@users.noreply.github.com>
2023-06-24 23:09:07 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,49 +1,39 @@
-﻿namespace SquareEquationLib1
+namespace SquareEquationLib;
+
+public class SquareEquation
 {
-    public class SquareEquation
+    public static double[] Solve(double a, double b, double c)
     {
-        public static double[] Solve(double a, double b, double c)
+        double eps = 1e-9;
+        double[] no_roots = new double[] { };
+        
+        
+        if (-eps < a && a < eps || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
         {
-            if (double.IsNaN(a) || double.IsInfinity(a))
-            {
-                throw new ArgumentException();
-            }
+            throw new ArgumentException();
+        }
 
-            if (double.IsNaN(b) || double.IsInfinity(b))
-            {
-                throw new ArgumentException();
-            }
-
-            if (double.IsNaN(c) || double.IsInfinity(c))
-            {
-                throw new ArgumentException();
-            }
-
-            if (a == 0)
-            {
-                throw new ArgumentException();
-            }
-
-            double discriminant = b * b - 4 * a * c;
-            double[] result;
-
-            if (discriminant > 0)
-            {
-                double x1 = (-b + Math.Sqrt(discriminant)) / (2 * a);
-                double x2 = (-b - Math.Sqrt(discriminant)) / (2 * a);
-                result = new double[] { x1, x2 };
-            }
-            else if (discriminant == 0)
-            {
-                double x = -b / (2 * a);
-                result = new double[] { x };
+        double D = b * b - 4 * a * c;
+        
+        if (D <= -eps) 
+        {
+            return no_roots;
+        }
+        else
+        {
+            if (-eps < D && D < eps)
+            { 
+                double[] root = new double[1];
+                root[0] = (-b) / 2 * a;
+                return root;
             }
             else
             {
-                result = new double[0];
+                double[] roots = new double[2];
+                roots[0] = -(b + Math.Sign(b) * Math.Sqrt(D)) / 2;
+                roots[1] = c / roots[0];
+                return roots;
             }
-
-            return result;
         }
     }
 }
