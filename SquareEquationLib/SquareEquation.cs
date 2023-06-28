3ec2cc4da8251ad5468adc4d1010ea27b7e4a6ca
Brassfoe402 <125663981+Brassfoe402@users.noreply.github.com>
2023-06-28 20:16:21 +0600

    Update SquareEquation.cs

--- SquareEquationLib/SquareEquation.cs
+++ SquareEquationLib/SquareEquation.cs
@@ -1,37 +1,37 @@
 namespace SquareEquationLib;
-
 public class SquareEquation
 {
     public static double[] Solve(double a, double b, double c)
     {
-        double x1;
-        double x2;
-        double[] array = new double[2];
+        double[] answer;
+        double x1, x2;
         double eps = 1e-9;
-        double d = b * b - 4 * a * c;
-        if (-eps < a && a < eps)
+        if ((-eps < a && a < eps)||
+            double.IsInfinity(a) || double.IsInfinity(b) ||
+            double.IsInfinity(c) || double.IsNaN(a) ||
+            double.IsNaN(b) || double.IsNaN(c))                                                 
         {
             throw new System.ArgumentException();
         }
-        if (a == 0 || new[] { a, b, c }.Any(double.IsNaN) || new[] { a, b, c }.Any(double.IsInfinity))
+        b = b/a;
+        c = c/a;
+        double discriminant = Math.Pow(b, 2) - 4 * c;
+        if (discriminant <= -eps)
         {
-            throw new ArgumentException("Ошибка");
+            answer = new double [0];
+            return answer;
         }
-        if (d<=-eps)
+        else if (Math.Abs(discriminant)<eps)
         {
-            array = new double[0];
+            answer = new double [1] { -b / 2 };
+            return answer;
         }
-        else if (-eps < d && d < eps)
+        else
         {
-            x1 = -b/(2*a);
-            array = new double[] { x1 };
+            x1 = -(b + Math.Sign(b) * Math.Sqrt(discriminant)) / 2;
+            x2 = c / x1;
+            answer = new double [2] { x1, x2 };
+            return answer;
         }
-        else 
-        {
-            x1 = (2*c)/-(b+Math.Sign(b)*Math.Sqrt(d));
-            x2 = c / (a*x1);
-            array = new double[] { x1, x2 };
-        }
-        return array;
     }
 }
